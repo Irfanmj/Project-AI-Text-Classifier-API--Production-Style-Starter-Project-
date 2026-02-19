3130b7321b5088ffacee231c3dd987a2658f2ca5
Irfan MJ <ahmadnurirfanmj@gmail.com>
2026-02-19 17:58:38 +0800

    upgraded sentiment model

--- model.py
+++ model.py
@@ -3,10 +3,12 @@ from transformers import pipeline #import pipeline from transformers library
 # load pretrained AI sentiment model
 classifier = pipeline("sentiment-analysis")
 
+pipeline("sentiment-analysis",
+         model="cardiffnlp/twitter-roberta-base-sentiment")
+
 # function to predict sentiment
 def predict(text):
     result = classifier(text)
     return result
 
-pipeline("sentiment-analysis",
-         model="cardiffnlp/twitter-roberta-base-sentiment")
+
